9e19507c465211c15b6c088bad4d2a41fc1a1add
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-06-19 09:08:27 -0500

    Adding animated icons

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+/* /* Icon Animation */
+const decryptImg = document.querySelector("#decorativeImg")
+const icons = ["assets/encryptImg.png", "assets/decryptImg.png"]
+let currentIndex = 0
 
+setInterval(() => {
+      currentIndex = (currentIndex + 1) % icons.length
+      decryptImg.src = icons[currentIndex]
+}, 1500)
+ */
+
+/* Adding sound into buttons */
 console.log("script cargado")
 const buttons = document.querySelectorAll("button");
 
@@ -15,12 +26,27 @@ const infoButton = document.getElementById("infoButton")
 const informationOverlay = document.getElementById("informationOverlay")
 const exitInformation = document.getElementById("exitInformation")
 
-infoButton.addEventListener("click", () =>{
+infoButton.addEventListener("click", () => {
       informationOverlay.classList.remove("hidden");
 })
 
 exitInformation.addEventListener("click", () => {
-  informationOverlay.classList.add("hidden");
+      informationOverlay.classList.add("hidden");
 });
 
+/* Input validation */
+const lowercaseOnly = /^[a-z\s]+$/;
+const
 
+function validateInput(text, password) {
+
+      if (!text.trim() || !lowercaseOnly.test(text)) {
+            alert("Please use only lowercase letters and spaces.")
+            return false;
+      }
+      if (!password || password.trim().length < 4) {
+            alert("Password must be at least 8 characters.")
+            return false
+      }
+      return true
+}
